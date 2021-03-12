a33826d54b917dd9f6ed40ae37eb377f5f8ad857
yardenas <yardas@student.ethz.ch>
2021-03-12 14:40:41 +0100

    Enable rendering

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -1430,7 +1430,7 @@ class Engine(gym.Env, gym.utils.EzPickle):
                height=DEFAULT_HEIGHT
                ):
         ''' Render the environment to the screen '''
-        return self.sim.render(height, width, camera_id=1)
+        return self.sim.render(height, width, camera_id='track', scene_option=self._vis_opt)
         # if self.viewer is None or mode!=self._old_render_mode:
         #     # Set camera if specified
         #     if mode == 'human':
